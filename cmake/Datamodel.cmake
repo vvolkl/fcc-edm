189dd5d1dd840e324478e8034176049cd07e6fdc
Valentin Volkl <valentin.volkl@cern.ch>
2019-01-06 21:01:57 +0000

    fcc-edm with only Float for compatibility with data

--- cmake/Datamodel.cmake
+++ cmake/Datamodel.cmake
@@ -6,31 +6,31 @@ include_directories(
 file(GLOB sources src/*.cc)
 file(GLOB headers datamodel/*.h podio/PythonEventStore.h)
 
-add_library(datamodel SHARED ${sources} ${headers})
-target_link_libraries(datamodel podio)
+add_library(datamodel-fcclegacy SHARED ${sources} ${headers})
+target_link_libraries(datamodel-fcclegacy podio)
 
-REFLEX_GENERATE_DICTIONARY(datamodel ${headers} SELECTION src/selection.xml )
-add_library(datamodelDict SHARED datamodel.cxx)
-add_dependencies(datamodelDict datamodel-dictgen)
-target_link_libraries(datamodelDict datamodel podio ${ROOT_LIBRARIES})
+REFLEX_GENERATE_DICTIONARY(datamodel-fcclegacy ${headers} SELECTION src/selection.xml )
+add_library(datamodel-fcclegacyDict SHARED datamodel-fcclegacy.cxx)
+add_dependencies(datamodel-fcclegacyDict datamodel-fcclegacy-dictgen)
+target_link_libraries(datamodel-fcclegacyDict datamodel-fcclegacy podio ${ROOT_LIBRARIES})
 
-set_target_properties(datamodel PROPERTIES
+set_target_properties(datamodel-fcclegacy PROPERTIES
   PUBLIC_HEADER "${headers}")
 
-install(TARGETS datamodel datamodelDict
-  # IMPORTANT: Add the datamodel library to the "export-set"
+install(TARGETS datamodel-fcclegacy datamodel-fcclegacyDict
+  # IMPORTANT: Add the datamodel-fcclegacy library to the "export-set"
   EXPORT fccedmTargets
   RUNTIME DESTINATION "${INSTALL_BIN_DIR}" COMPONENT bin
   LIBRARY DESTINATION "${INSTALL_LIB_DIR}" COMPONENT shlib
-  PUBLIC_HEADER DESTINATION "${INSTALL_INCLUDE_DIR}/datamodel"
+  PUBLIC_HEADER DESTINATION "${INSTALL_INCLUDE_DIR}/datamodel-fcclegacy"
   COMPONENT dev)
 
 install(FILES
-  "${PROJECT_BINARY_DIR}/datamodel/datamodelDict.rootmap"
+  "${PROJECT_BINARY_DIR}/datamodel-fcclegacy/datamodel-fcclegacyDict.rootmap"
   DESTINATION "${INSTALL_LIB_DIR}" COMPONENT dev)
 
 if (${ROOT_VERSION} GREATER 6)
   install(FILES
-      "${PROJECT_BINARY_DIR}/datamodel/datamodel_rdict.pcm"
+      "${PROJECT_BINARY_DIR}/datamodel-fcclegacy/datamodel-fcclegacy_rdict.pcm"
       DESTINATION "${INSTALL_LIB_DIR}" COMPONENT dev)
 endif()
